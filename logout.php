d6e61556b7bcc29fb86aabe4cfbc5a38d9beb0d6
Brahman Singh <brahman4028@gmail.com>
2025-07-11 15:57:21 +0530

    second changes

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();     // Optional: clears session variables
-session_destroy();   // Destroys session data
-
-header("Location: login.php"); // Redirect after logout
-exit();
