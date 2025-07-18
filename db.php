4934fbe18b9cf8453b59801622b03dfd907c167e
Suraj Pandey <brahman4028@gmail.com>
2025-07-18 15:20:53 +0530

    suraj 7 pm changessg

--- db.php
+++ db.php
@@ -1,7 +1,19 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "onboarding_merchants");
+// $mysqli = new mysqli("localhost", "root", "", "onboarding_merchants");
+
+// if ($mysqli->connect_error) {
+//     die("Connection failed: " . $mysqli->connect_error);
+// }
+
+
+// <!-- server db -->
+
+$mysqli = new mysqli("localhost", "u800867687_onboardingpage", "Onboarding@4028", "u800867687_onboardingpage");
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
+
+
 ?>
+
