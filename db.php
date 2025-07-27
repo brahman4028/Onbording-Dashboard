cd86f52d434f65ff7a0426dc06215fac79949068
Suraj Pandey <brahman4028@gmail.com>
2025-07-27 19:05:27 +0530

    production level 1

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "onboarding_merchants");
+// $mysqli = new mysqli("localhost", "root", "", "onboarding_merchants");
 
 // if ($mysqli->connect_error) {
 //     die("Connection failed: " . $mysqli->connect_error);
