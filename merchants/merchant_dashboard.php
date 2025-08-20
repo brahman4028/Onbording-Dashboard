ab64fcde009cd0b8c9374c3045824a410fee7960
Suraj Pandey <brahman4028@gmail.com>
2025-08-20 13:43:28 +0530

    production 3

--- merchants/merchant_dashboard.php
+++ merchants/merchant_dashboard.php
@@ -21,7 +21,7 @@ $merchantuseremail = $_SESSION['merchant_info']['email'];
 // echo $application_id;
 
 if ($application_id != '') {
-    $appQuery = "SELECT * FROM business_applications WHERE id = $application_id";
+    $appQuery = "SELECT * FROM business_applications WHERE id = '$application_id'";
     $appResult = mysqli_query($mysqli, $appQuery);
 
     if (!$appResult || mysqli_num_rows($appResult) === 0) {
