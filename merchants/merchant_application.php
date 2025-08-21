62f20914270dd82dc25b743399d2b837397b9868
Suraj Pandey <brahman4028@gmail.com>
2025-08-21 18:18:55 +0530

    production 4

--- merchants/merchant_application.php
+++ merchants/merchant_application.php
@@ -992,7 +992,9 @@ $docData = $docResult ? mysqli_fetch_assoc($docResult) : [];
                                                             <a href="<?= $aadhaarUrl ?>" target="_blank">
                                                                 View uploaded file
                                                             </a>
-                                                            <div id="aadhaarfilepreview" data-fileurl="<?= $aadhaarUrl ?>"></div>
+
+                                                            <div id="aadhaarfilepreviw" data-fileurl="<?= $aadhaarUrl ?>"></div>
+
                                                         <?php else: ?>
                                                             <p style="color: #888;">No file uploaded</p>
                                                         <?php endif; ?>
